a0036bdcf78c8f7171cb2da0d3491f9408174f61
puteradarmas <putera9aksel2014@gmail.com>
2023-09-30 16:02:51 +0700

    commit

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import todo
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title','description','completed')
+    
+admin.site.register(todo,TodoAdmin)
+
+
--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class todo(models.Model):
+    
+    title = models.CharField(max_length = 150)
+    description = models.TextField()
+    completed = models.BooleanField(default= False)
+    
+    def __str__(self):
+        return self.title
+    
+    
+    
+    
+    
